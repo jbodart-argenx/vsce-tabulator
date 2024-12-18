6d770c674f4434f09d75b8564138eac42421a835
Jean-Michel Bodart <jbodart@argenx.com>
2024-12-18 09:07:25 +0100

    Update extension.js
    Fix path to webview.html
    Format document

--- src/extension.js
+++ src/extension.js
@@ -10,11 +10,11 @@ function activate(context) {
 			vscode.ViewColumn.One,
 			{
 				enableScripts: true,
-				localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'media'))],
+				localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'src'))],
 			}
 		);
 
-		const htmlPath = path.join(context.extensionPath, 'media', 'index.html');
+		const htmlPath = path.join(context.extensionPath, 'src', 'webview.html');
 		const htmlContent = fs.readFileSync(htmlPath, 'utf8');
 		panel.webview.html = htmlContent;
 
@@ -39,12 +39,12 @@ function deactivate() { }
 function getData() {
 	// Replace with your data retrieval logic
 	return [
-		{id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-		{id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-		{id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-		{id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-		{id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-		{id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
+		{ id: 1, name: "Oli Bob", progress: 12, gender: "male", rating: 1, col: "red", dob: "19/02/1984", car: 1 },
+		{ id: 2, name: "Mary May", progress: 1, gender: "female", rating: 2, col: "blue", dob: "14/05/1982", car: true },
+		{ id: 3, name: "Christine Lobowski", progress: 42, gender: "female", rating: 0, col: "green", dob: "22/05/1982", car: "true" },
+		{ id: 4, name: "Brendon Philips", progress: 100, gender: "male", rating: 1, col: "orange", dob: "01/08/1980" },
+		{ id: 5, name: "Margret Marmajuke", progress: 16, gender: "female", rating: 5, col: "yellow", dob: "31/01/1999" },
+		{ id: 6, name: "Frank Harbours", progress: 38, gender: "male", rating: 4, col: "red", dob: "12/05/1966", car: 1 },
 		// Add more rows as needed
 	];
 }
