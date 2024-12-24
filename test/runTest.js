557c47e25efc724e9d81d3e7466fd7c207d90f62
Jean-Michel Bodart <jbodart@argenx.com>
2024-12-24 08:35:51 +0100

    Update test/runTest.js
    Fix extensionTestsPath
    Explicitly specify testWorkspace

--- test/runTest.js
+++ test/runTest.js
@@ -10,11 +10,24 @@ async function main() {
 
       // The path to the extension test script
       // Passed to --extensionTestsPath
-      const extensionTestsPath = path.resolve(__dirname, './suite/index.js');
+      const extensionTestsPath = path.resolve(__dirname, './suite');
       console.log(`(test/runTest.js) extensionTestsPath:`, extensionTestsPath);
 
+      const testWorkspace = path.resolve(__dirname, './test-workspace');
+
       // Download VS Code, unzip it and run the integration test
-      await runTests({ extensionDevelopmentPath, extensionTestsPath });
+      await runTests({
+         extensionDevelopmentPath,
+         extensionTestsPath,
+         launchArgs: [
+            testWorkspace,
+            // '--disable-extensions',
+         ], 
+			// Custom environment variables for extension test script
+			// extensionTestsEnv: { foo: 'bar' },
+         // platform: process.platform === 'win32' ? 'win32-x64-archive' : process.platform,
+      });
+
    } catch (err) {
       console.error('Failed to run tests:', err.message);
       process.exit(1);
