49b164f31af00b6a40b460edbda41130fabf68fd
Jean-Michel Bodart <jbodart@argenx.com>
2024-12-24 08:33:38 +0100

    Update test/suite/index.js
    Fix incorrect run() function
    Log testsRoot and test files found

--- test/suite/index.js
+++ test/suite/index.js
@@ -2,41 +2,41 @@ const path = require('path');
 const Mocha = require('mocha');
 const { glob } = require('glob');
 
-function run() {
+function run(testsRoot, cb) {
    // Create the Mocha test
    const mocha = new Mocha({
       ui: 'tdd',
       color: true,
    });
 
-   const testsRoot = path.resolve(__dirname, '..');
+   if (!testsRoot) {
+      testsRoot = path.resolve(__dirname, '..');
+   }
+   console.log('(test/suite/index.js) testsRoot:', testsRoot);
 
-   return new Promise((resolve, reject) => {
-      glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-         if (err) {
-            return reject(err);
-         }
-
-         // Add files to the test suite
-         files.forEach((file) => {
-            console.log(`Adding test file: ${file}`);
-            mocha.addFile(path.resolve(testsRoot, file));
+   glob('**/**.test.js', { cwd: testsRoot })
+		.then((files) => {
+         console.log(`(test/suite/index.js) Found ${files.length} test files:`, files);
+			// Add files to the test suite
+			files.forEach((f) => {
+            console.log(`Adding test file: ${f}`);
+            mocha.addFile(path.resolve(testsRoot, f));
          });
 
-         try {
-            // Run the mocha test
-            mocha.run((failures) => {
-               if (failures > 0) {
-                  reject(new Error(`${failures} tests failed.`));
-               } else {
-                  resolve();
-               }
-            });
-         } catch (err) {
-            reject(err);
-         }
-      });
-   });
+			try {
+				// Run the mocha test
+				mocha.run((failures) => {
+					cb(null, failures);
+				});
+			} catch (err) {
+				console.error(err);
+				cb(err);
+			}
+		})
+		.catch((err) => {
+			return cb(err);
+		});
+
 }
 
 module.exports = {
